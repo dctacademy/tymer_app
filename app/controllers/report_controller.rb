912f231cfb163917cfabb8c88c72c61b2880582e
Aniruddha SG <aniruddha.s.g@gmail.com>
2016-07-30 22:55:42 +0530

    income chart

--- app/controllers/report_controller.rb
+++ app/controllers/report_controller.rb
@@ -5,7 +5,9 @@ class ReportController < ApplicationController
   end
 
   def income
-  	@batches = Batch.all
-  	@batch_years = @batches.group_by {|batch| batch.start_date.beginning_of_month }
+  	@payments_2015 = Payment.where('payment_date >= ? AND payment_date <= ?', Date.parse("2015-01-01"), Date.parse("2015-12-31"))
+  	@payments_2016 = Payment.where('payment_date >= ? AND payment_date <= ?', Date.parse("2016-01-01"), Date.parse("2016-12-31"))
+  	@payments_by_month_2015 = @payments_2015.group_by {|payment| payment.payment_date.beginning_of_month}	
+  	@payments_by_month_2016 = @payments_2016.group_by {|payment| payment.payment_date.beginning_of_month}	
   end
 end
