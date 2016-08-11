6511edfdb402d0ea31a25dc8c41194012aab9ae9
Aniruddha SG <aniruddha.s.g@gmail.com>
2016-08-11 12:44:29 +0530

    with validation

--- app/models/student.rb
+++ app/models/student.rb
@@ -19,7 +19,7 @@ class Student < ActiveRecord::Base
   validates :name, presence: true
   #validates_uniqueness_of :mobile, scope: :user_id
   validates_length_of :mobile, is: 10, allow_blank: true
-  validate :check_mobile
+  #validate :check_mobile
   
   def check_mobile
     unless self.learning_style == 'workshop'
