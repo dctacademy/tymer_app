141a0b5fe6f133a5185ea45cfee6604fb220c0ff
Aniruddha SG <aniruddha.s.g@gmail.com>
2016-09-16 11:36:01 +0530

    with stats updated for 2014

--- app/controllers/report_controller.rb
+++ app/controllers/report_controller.rb
@@ -1,6 +1,6 @@
 class ReportController < ApplicationController
   def by_year
-  	@batches = Batch.all
+  	@batches = Batch.order('start_date')
   	@batch_years = @batches.group_by {|batch| batch.start_date.beginning_of_year}
   end
 
