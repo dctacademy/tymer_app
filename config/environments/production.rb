94d79edfbb5bdb8f2089624b2a398eda2ccbb327
= <ani@dctacademy.com>
2016-11-30 11:35:51 +0530

    with code to pre

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -87,5 +87,5 @@ Rails.application.config.middleware.use ExceptionNotification::Rack,
     :deliver_with => :deliver, # Rails >= 4.2.1 do not need this option since it defaults to :deliver_now
     :email_prefix => "App Crashed",
     :sender_address => %{"notifier" <notifier@example.com>},
-    :exception_recipients => %w{aniruddhas.g@gmail.com}
+    :exception_recipients => %w{aniruddha.s.g@gmail.com}
   }
